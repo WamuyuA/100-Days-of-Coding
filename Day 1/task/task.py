31731515a6414385113997296715c0931eeafac3
WamuyuA <annettewamuyu94@gmail.com>
2025-04-07 10:48:15 +0930

    First task

---
+++ Day 1/task/task.py
@@ -0,0 +1,5 @@
+#Band name generator
+print("Hi, welcome to Band Name Generator App!")
+city_name = input("What city did you grow up in?" + "\n")
+pet_name = input("What is your favorite pet's name?" + "\n")
+print("Your band name would be "  +"\n" +  city_name +  " " + pet_name)
